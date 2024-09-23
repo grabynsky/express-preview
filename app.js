924eba908b7c809c7fe0db1f865daceb0ba2a787
Andrew Grabynsky <hrabynsky@gmail.com>
2024-09-23 16:37:08 +0300

    23092024 add user

--- app.js
+++ app.js
@@ -1,77 +1,29 @@
 const express = require('express');
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const port = 3000
+const users = [
+    {id:1, name: 'Maksym', email:'feden@gmail.com', password: 'qwe123'},
+    {id:2, name: 'Alina', email:'alinados@gmail.com', password: 'ert123'},
+];
 
-const hubkaBob = [
-    {
-        name: 'Hubka Bob',
-        age: 18,
-        gender: 'male'
-    },
-    {
-        name: 'Hubka Bob',
-        age: 16,
-        gender: 'other'
-    },
-    {
-        name: 'Hubka Bob',
-        age: 5,
-        gender: 'other'
-    },
-    {
-        name: 'Hubka Bob',
-        age: 20,
-        gender: 'male'
-    },
-    {
-        name: 'Hubka Bob',
-        age: 19,
-        gender: 'other'
-    },
-]
-
-app.get('/hubkaBob', (req, res) => {
-    // res.send('Hello World!')
-    res.json(hubkaBob)
+app.get('/users', (req, res) => {
+    res.send('Hello users');
 })
 
-app.post('/hubkaBob', (req, res) => {
-    // console.log('Post request to HubkaBob')
-    // console.log(req.body)
-
-    const hubkabob = req.body;
-    hubkaBob.push(hubkabob);
-    res.status(201).json({message: 'Hubka Bob Created'})
+app.post('/users', (req, res) => {
+    console.log('body', req.body)
+    console.log('query', req.query)
+    console.log('params', req.params)
+    res.send('Hello users 2');
 })
 
-app.put('/hubkaBob/:id', (req, res) => {
-   const {id} = req.params;
-
-   updatedHubkaBobInfo = req.body;
-
-   hubkaBob[+id] = updatedHubkaBobInfo;
-
-   res.status(200).json({
-       message: 'Hubka Bob update successfully',
-       data: hubkaBob[id]
-   })
+app.post('/users/:userId', (req, res) => {
+    console.log('params', req.params)
+    res.send('Hello userId');
 })
 
-app.delete('/hubkaBob/:id', (req, res) => {
-    const {id} = req.params;
-
-    hubkaBob.splice(+id, 1);
-
-    res.status(200).json({
-        message: 'Hubka Bob delete'
-    })
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-
-})
+app.listen(3000)
