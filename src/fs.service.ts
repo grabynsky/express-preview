ae46f6efc56c3650ed83beb0a2d4e85dcea960b9
Andrew Grabynsky <hrabynsky@gmail.com>
2024-09-24 15:20:03 +0300

    start use eslint

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,18 +1,19 @@
-import path from "node:path";
 import fs from "node:fs/promises";
-import {IUser} from "./interfaces/user.interface";
+import path from "node:path";
+
+import { IUser } from "./interfaces/user.interface";
 
 const read = async (): Promise<IUser[]> => {
-    const pathToFile = path.join(process.cwd(), 'db.json');
-    const data = await fs.readFile(pathToFile, "utf-8");
+  const pathToFile = path.join(process.cwd(), "db.json");
+  const data = await fs.readFile(pathToFile, "utf-8");
 
-    return data ? JSON.parse(data) : [];
+  return data ? JSON.parse(data) : [];
 };
 
 const write = async (users: IUser[]): Promise<void> => {
-    const pathToFile = path.join(process.cwd(), 'db.json');
+  const pathToFile = path.join(process.cwd(), "db.json");
 
-    await fs.writeFile(pathToFile, JSON.stringify(users))
+  await fs.writeFile(pathToFile, JSON.stringify(users));
 };
 
 export { read, write };
